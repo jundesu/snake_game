df594dbb4c392d27595cdacee215c0d5b90db84c
Grace <hello@localhost>
2021-08-09 16:41:47 +0800

    implement board component and css style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React, { useState } from 'react';
+
+const width = 15;
+const height = 15;
+
 function Game() {
   const [score,setScore] = useState(0);
 
   return (
     <div className="game">
       <ScoreBoard score={score}/>
+      <Board />
     </div>
   );
 }
@@ -13,7 +18,32 @@ function Game() {
 function ScoreBoard(props) {
   return (
     <div className="scoreBoard">
-      Score<span>{props.score}</span>
+      Score
+      <span>{props.score}</span>
+    </div>
+  );
+}
+
+function Board() {
+  const columnArr = (new Array(width)).fill(null);
+  for(let i = 0; i < columnArr.length; i++) {
+    const rowArr = (new Array(height)).fill(null);
+    columnArr[i] = rowArr;
+  }
+
+  return (
+    <div className="board">
+     {
+       columnArr.map((column, x) => {
+        return (
+          column.map((cell, y) => {
+            return (
+              <div className="unit"></div>
+            );
+          })
+        );
+       })
+     }
     </div>
   );
 }
