64e2aa56671b84c929759811c9b80173e86869ba
Grace <hello@localhost>
2021-08-09 17:33:43 +0800

    display food and snake on board

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ const height = 15;
 
 function Game() {
   const [score,setScore] = useState(0);
+  const [food, setFood] = useState([10,10]);
+  const [snake, setSnake] = useState([[7,4], [6,4], [5,4]]);
 
+  
   return (
     <div className="game">
       <ScoreBoard score={score}/>
-      <Board />
+      <Board snake={snake} food={food} />
     </div>
   );
 }
@@ -24,21 +27,38 @@ function ScoreBoard(props) {
   );
 }
 
-function Board() {
+function Board(props) {
   const columnArr = (new Array(width)).fill(null);
   for(let i = 0; i < columnArr.length; i++) {
     const rowArr = (new Array(height)).fill(null);
     columnArr[i] = rowArr;
   }
+  
+  const isFood = 1;
+  const isSnake = 2;
 
+  const[foodX, foodY]= props.food;
+  columnArr[foodX][foodY] = isFood;
+
+  props.snake.forEach(snakeArr => {
+    const [snakeX,snakeY] = snakeArr;
+    columnArr[snakeX][snakeY] = isSnake;
+  });
+ 
   return (
     <div className="board">
      {
        columnArr.map((column, x) => {
         return (
           column.map((cell, y) => {
+            if(cell === isFood) {
+              return (<div className="unit food"></div>)
+            }
+            if(cell === isSnake) {
+                return (<div className="unit snake"></div>)
+            }
             return (
-              <div className="unit"></div>
+            <div className="unit"></div>
             );
           })
         );
