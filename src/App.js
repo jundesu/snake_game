3c8ff3631c1b66733257eb680f3f707468b210f0
Grace <hello@localhost>
2021-08-11 16:23:20 +0800

    move snake and  control direction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const width = 15;
 const height = 15;
@@ -8,8 +8,43 @@ function Game() {
   const [score,setScore] = useState(0);
   const [food, setFood] = useState([10,10]);
   const [snake, setSnake] = useState([[7,4], [6,4], [5,4]]);
+  const [direction, setDirection] = useState([1,0]);
+  const [delay, setDelay] = useState(500);
 
-  
+  useInterval(() => {
+    const head = snake[0];
+    const newHead = [head[0] + direction[0], head[1] + direction[1]];
+    const newSnake = snake.slice();
+    newSnake.unshift(newHead);
+    newSnake.pop();
+    setSnake(newSnake);
+  }, delay);
+
+  useListener('keydown', (e) => {
+    switch(e.code) {
+      case 'ArrowUp':
+        if(direction[0] !== 0) {
+          setDirection([0, -1]);
+        }
+        break;
+      case 'ArrowDown':
+        if(direction[0] !== 0) {
+          setDirection([0, 1]);
+        }
+        break;
+      case 'ArrowLeft':
+        if(direction[1] !== 0) {
+          setDirection([-1, 0]);
+        }
+        break;
+      case 'ArrowRight':
+        if(direction[1] !== 0) {
+          setDirection([1, 0]);
+        }
+        break;
+    }
+  });
+
   return (
     <div className="game">
       <ScoreBoard score={score}/>
@@ -51,14 +86,15 @@ function Board(props) {
        columnArr.map((column, x) => {
         return (
           column.map((cell, y) => {
+            const k = `${x}${y}`
             if(cell === isFood) {
-              return (<div className="unit food"></div>)
+              return (<div key={k} className="unit food"></div>)
             }
             if(cell === isSnake) {
-                return (<div className="unit snake"></div>)
+                return (<div key={k} className="unit snake"></div>)
             }
             return (
-            <div className="unit"></div>
+            <div key={k} className="unit"></div>
             );
           })
         );
@@ -67,6 +103,41 @@ function Board(props) {
     </div>
   );
 }
+
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+    
+  },[delay]);
+}
+
+const useListener = (type, callback) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const handle = (e) => {
+      savedCallback.current(e);
+    };
+    document.addEventListener(type, handle);
+    return () => document.addEventLlistener(type, handle)
+    
+  }, [type]);
+}
+
 
 function App() {
   return (
